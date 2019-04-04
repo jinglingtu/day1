92f2f6b1b35c20cdce790637e65357e11eb09fbd
luolijie <2456217855@qq.com>
2019-04-04 10:25:21 +0800

    day1xiangmu

---
+++ day1/src/demo01/demo02MethodDefie.java
@@ -0,0 +1,17 @@
+package demo01;
+
+public class demo02MethodDefie {
+	public  static void  main(String[]  args){
+		System.out.println(add(10,30));
+		 System.out.println("______");
+		 int k=add(10,30);
+		 System.out.println(k);
+	}
+	
+	public static int add(int a,int b){
+		int result=a+b;
+		return result;
+		
+	}
+
+}
---
+++ day1/src/demo01/demo1.java
@@ -0,0 +1,15 @@
+package demo01;
+
+public class demo1 {
+	public static void main(){
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<10;j++)
+			{
+				System.out.println("*");
+				
+			}
+		}
+	}
+
+}
